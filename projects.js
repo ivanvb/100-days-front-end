78bcac0c0f312491a2288592e9b0c67b4d3b0f7d
ivb98 <33399537+ivb98@users.noreply.github.com>
2020-06-11 15:33:42 +0000

    Deploying to gh-pages from  @ 8ad3057baec03c6d536e2d0482c669ad8f935774 🚀

--- projects.js
+++ projects.js
@@ -1,5 +1,5 @@
 
         const container = document.getElementById('container');
-        const links = ['<a href="./days/Day-001/index.html">Text Vertical Scroller</a>','<a href="./days/Day-002/index.html">Spinning Circles</a>','<a href="./days/Day-003/index.html">Day Cycle 🌞</a>','<a href="./days/Day-004/index.html">Pulse Loading Animation</a>']
+        const links = ['<a href="./days/Day-001/index.html">Text Vertical Scroller</a>','<a href="./days/Day-002/index.html">Spinning Circles</a>','<a href="./days/Day-003/index.html">Day Cycle 🌞</a>','<a href="./days/Day-004/index.html">Pulse Loading Animation</a>','<a href="./days/Day-005/index.html">Click and Squeeze</a>']
         container.innerHTML = links.join("\n")
     
