04dc49e8290086989395de7d1a8d8710cf7d5ad8
ivb98 <ivanvalette@hotmail.com>
2020-06-16 10:52:50 -0400

    day10: improve mobile UX
    Made sidenav button wider on mobile.
    Website's overflow is now hidden when the navbar is shown.

--- public/days/Day-010/index.js
+++ public/days/Day-010/index.js
@@ -2,14 +2,17 @@ window.onload = () => {
     const button = document.getElementById('sidenav-button');
     const [sidenav] = document.getElementsByClassName('sidenav');
     const [cover] = document.getElementsByClassName('cover');
+    const [body] = document.getElementsByTagName('body');
 
     button.addEventListener('click', () => {
         sidenav.style.transform = 'translate(0%)';
         cover.style.display = 'block';
+        body.style.overflow = 'hidden';
     });
 
     cover.addEventListener('click', () => {
         sidenav.style.transform = 'translate(-150%)';
         cover.style.display = 'none';
+        body.style.overflow = 'auto';
     });
 };
