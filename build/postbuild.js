e188c10e79fba35435d6ac698f81c3bef9c35468
ivb98 <ivanvalette@hotmail.com>
2020-06-27 19:48:11 -0400

    add postbuild script
    build: add postbuild script
    
    This script fixes the path resolved by Parcel after it built

---
+++ build/postbuild.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+let builtHtml = fs.readFileSync(path.resolve(__dirname, '../dist/index.build.html'), 'utf8');
+builtHtml = builtHtml.replace(/\.\./g, '.');
+fs.writeFileSync(path.resolve(__dirname, '../dist/index.html'), builtHtml, 'utf8');
+fs.unlinkSync(path.resolve(__dirname, '../dist/index.build.html'));
