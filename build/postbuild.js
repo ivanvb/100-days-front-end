b28f1492215d806c5b3255e64438f5973ef6dba9
ivb98 <ivanvalette@hotmail.com>
2020-06-27 19:54:17 -0400

    fix: relative path in output files for Parcel

--- build/postbuild.js
+++ build/postbuild.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 let builtHtml = fs.readFileSync(path.resolve(__dirname, '../dist/index.build.html'), 'utf8');
-builtHtml = builtHtml.replace(/\.\./g, '.');
+builtHtml = builtHtml.replace(/\.\.\/\.\./g, '.');
 fs.writeFileSync(path.resolve(__dirname, '../dist/index.html'), builtHtml, 'utf8');
 fs.unlinkSync(path.resolve(__dirname, '../dist/index.build.html'));
