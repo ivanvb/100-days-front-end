d7b8e1be33bcb6b2a22d3631fe737b7a4e203058
ivb98 <ivanvalette@hotmail.com>
2020-07-01 16:52:34 -0400

    fix: Mobile menu

--- src/days/Day-025/index.js
+++ src/days/Day-025/index.js
@@ -3,9 +3,7 @@ const [lineTop, , lineBottom] = container.children;
 let isOpen = false;
 
 container.addEventListener('click', (e) => {
-    if (e.path[0].nodeName !== 'A') {
-        container.classList.remove(isOpen ? 'active' : 'inactive');
-        container.classList.add(isOpen ? 'inactive' : 'active');
-        isOpen = !isOpen;
-    }
+    container.classList.remove(isOpen ? 'active' : 'inactive');
+    container.classList.add(isOpen ? 'inactive' : 'active');
+    isOpen = !isOpen;
 });
