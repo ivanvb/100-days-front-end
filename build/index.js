6db708a7a6d805c8d2149f147d4892fb159ba731
ivb98 <ivanvalette@hotmail.com>
2020-07-03 21:07:57 -0400

    feat: Projects using JS now indicate it in the index page
    The build script was update to automatically add the 'js' class to
    each project that contains a <script> tag

--- build/index.js
+++ build/index.js
@@ -15,18 +15,24 @@ function setDirectoriesMetadata(daysDirectories) {
         const indexPath = `${BASE_PATH}${directory}/index.html`;
         const html = fs.readFileSync(indexPath, 'utf8');
         const [title] = /(?<=<title>).*(?=<\/title>)/.exec(html);
+        const hasJs = /<script.*>.*<\/script>/.exec(html);
 
         directoriesMetadata.push({
             title,
             day: directory.split('-')[1],
             path: indexPath.replace('src/', ''),
+            hasJs: !!hasJs,
         });
     }
 }
 
 function createDirectoriesScript() {
     const projectsHTML = directoriesMetadata.map((day) => {
-        return `<div class="project"><h2 class="title">${day.title}</h2><h3 class="day">Day ${day.day}</h3><a class="project-link" href="${day.path}">View Project</a></div>`;
+        return `<div class="project ${day.hasJs ? 'js' : ''}"><h2 class="title">${
+            day.title
+        }</h2><h3 class="day">Day ${day.day}</h3><a class="project-link" href="${
+            day.path
+        }">View Project</a></div>`;
     });
     let htmlIndex = fs.readFileSync(path.resolve(__dirname, '../src/index.html'), 'utf8');
     htmlIndex = htmlIndex.replace('<div id="container">', (match) => {
