279a04bad9ac72ab838fa037d539e7fb41320102
ivanvb <ivanvalette@hotmail.com>
2020-09-01 20:27:10 -0400

    day87: rotation controls

---
+++ src/days/Day-087/cubeControls.js
@@ -0,0 +1,61 @@
+let yPos = 65;
+let zPos = 0;
+
+// Directions
+const UP = 0;
+const DOWN = 1;
+const LEFT = 2;
+const RIGHT = 3;
+
+const CUBE_DIRECTIONS = {
+    KeyA: LEFT,
+    KeyD: RIGHT,
+    KeyW: UP,
+    KeyS: DOWN,
+};
+
+document.addEventListener('keydown', (e) => {
+    rotateCube(CUBE_DIRECTIONS[e.code]);
+});
+
+const cubeUpButton = document.querySelector('.cube-up');
+const cubeDownButton = document.querySelector('.cube-down');
+const cubeLeftButton = document.querySelector('.cube-left');
+const cubeRightButton = document.querySelector('.cube-right');
+
+const cubeMovement = [
+    {
+        element: cubeUpButton,
+        direction: UP,
+    },
+    {
+        element: cubeDownButton,
+        direction: DOWN,
+    },
+    {
+        element: cubeLeftButton,
+        direction: LEFT,
+    },
+    {
+        element: cubeRightButton,
+        direction: RIGHT,
+    },
+];
+
+cubeMovement.forEach(({ direction, element }) => {
+    element.addEventListener('click', () => rotateCube(direction));
+});
+
+function rotateCube(direction) {
+    if (direction === LEFT) {
+        yPos += 90;
+    } else if (direction === RIGHT) {
+        yPos -= 90;
+    } else if (direction === UP) {
+        zPos += 90;
+    } else if (direction === DOWN) {
+        zPos -= 90;
+    }
+
+    cube.style.transform = `rotateY(${yPos}deg) rotateZ(${zPos}deg)`;
+}
